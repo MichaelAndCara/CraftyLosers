ad524ba360c963cecc1e5968b79dcfa19b734465
MichaelAndCara <michaelandcara@gmail.com>
2012-01-02 23:45:54 -0600

    first commit

---
+++ CraftyLosers/Models/WeightCheckInsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CraftyLosers.Models
+{
+    public class WeightCheckInsModel
+    {
+
+    }
+}
