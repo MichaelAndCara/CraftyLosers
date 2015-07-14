2b729da57800564cc6e81123dd776de094ae9db9
MichaelAndCara <michaelandcara@gmail.com>
2015-07-14 08:28:20 -0500

    Update CraftyContext.cs

--- CraftyLosers/Repositories/CraftyContext.cs
+++ CraftyLosers/Repositories/CraftyContext.cs
@@ -11,8 +11,8 @@ namespace CraftyLosers.Repositories
     public class CraftyContext : DbContext
     {
         public CraftyContext()
-            //: base(@"Data Source=.\sqlexpress;Initial Catalog=BigIsLoser;Persist Security Info=True;MultipleActiveResultSets=True;User ID=macdbuser;Password=cjogre77")
-            : base(@"Data Source=localhost;Initial Catalog=BigIsLoser;Persist Security Info=True;MultipleActiveResultSets=True;User ID=macdbuser;Password=cjogre77")
+            //: base(@"Data Source=.\sqlexpress;Initial Catalog=BigIsLoser;Persist Security Info=True;MultipleActiveResultSets=True;User ID=*******;Password=*******")
+            : base(@"Data Source=localhost;Initial Catalog=BigIsLoser;Persist Security Info=True;MultipleActiveResultSets=True;User ID=*******;Password=*******")
         {
             
         }
